07fb39e61d5318a907f58125feb7c1bd9f4f2262
shashank <shashanksinghrocks99@gmail.com>
2020-03-28 22:32:50 +0530

    made changes to run on server

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import express from 'express';
+var express = require("express");
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", "ejs")
 
-app.get("/",()=> {
+app.get("/",(req, res)=> {
     res.render("index.ejs")
 })
 
-app.listen(process.env.PORT, ()=> {
-    // console.log("Server Started");
+app.listen(8080, ()=> {
+    console.log("Server Started");
 })
